e57b5e6a488850842b0775c3faaafb1930e5e21e
Lolpawer <dadu.petrosino@gmail.com>
2022-01-24 15:31:08 +0100

    emails generated and printed

--- js/script.js
+++ js/script.js
@@ -2,4 +2,32 @@ new Vue ({
 
     el: '#app',
 
+    data: {
+
+        emailList: [],
+
+    },
+
+    methods: {
+
+        generateEmail: function () {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+
+            this.emailList.push(email.data.response);
+
+            });
+
+        },
+
+    },
+
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            this.generateEmail();
+        }
+
+    },
+
 });
